34d45812ba1aa9168093169c47815b789943cbd8
Victoria Hernandez <vlhernan@meta.com>
2025-06-06 11:20:01 -0700

    d'

--- script.js
+++ script.js
@@ -10,16 +10,44 @@ const loadReviews = () => {
     }
 }
 
-// once content is loaded, then call loadReviews
-document.addEventListener("DOMContentLoaded", loadReviews);
 
+// our submit handler
+const handleReviewSubmit = (event) => {
+    console.log("handleReviewSubmit called")
+    event.preventDefault(); // making sure there are things to submit
 
-const handleReviewSubmit = () => {
-    console.log("handleReviewSubmit called");
+    const reviewInput = document.querySelector("#review-text");
+    const reviewText = reviewInput.value;
+
+    const reviewRatingInput = document.querySelector("#rating");
+    const rating = reviewRatingInput.value;
+
+    const reviewTitle = document.querySelector("#book-title");
+    const title = reviewTitle.value;
+
+    // create a new review object
+    let review = {
+        title: title,
+        reviewText: reviewText,
+        rating: rating,
+        likes: 0,
+        reposts: 0
+    }
+
+    const newReviewElement = createReviewElement(review);
+    const reviewsContainer = document.querySelector("#reviews-list");
+
+    reviewsContainer.insertBefore(newReviewElement, reviewsContainer.firstChild); // insert at the top of the list
+    event.target.reset(); // reset the form
+
 }
 
-const submitButton = document.querySelector("button");
-submitButton.addEventListener("click", handleReviewSubmit);
+// once content is loaded, then call loadReviews
+document.addEventListener("DOMContentLoaded", () =>{
+    loadReviews()
+    const reviewForm = document.querySelector("#review-form");
+    reviewForm.addEventListener("submit", handleReviewSubmit);
+});
 
 const createReviewElement = (review) => {
     console.log(review);
